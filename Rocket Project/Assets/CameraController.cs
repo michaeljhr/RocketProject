2ccc4bf3a2d586929b3ce261bf0e074be16d1da0
michaeljhr <99460971+michaeljhr@users.noreply.github.com>
2024-07-25 21:08:06 -0400

    rocket manual control

---
+++ Rocket Project/Assets/CameraController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target; // The target object to orbit around
+    public Vector3 offset = Vector3.zero; // Offset from the target's position
+    public float distance = 10.0f; // Initial distance from the target
+    public float xSpeed = 250.0f; // Speed of the camera's horizontal rotation
+    public float ySpeed = 120.0f; // Speed of the camera's vertical rotation
+    public float yMinLimit = -20f; // Minimum vertical angle
+    public float yMaxLimit = 80f; // Maximum vertical angle
+    public float zoomSpeed = 10.0f;
+    public float distanceMin = 3f; // Minimum zoom distance
+    public float distanceMax = 15f; // Maximum zoom distance
+    public float panSpeed = 0.3f; // Speed of the panning movement
+
+    private float x = 0.0f; // Current horizontal angle
+    private float y = 0.0f; // Current vertical angle
+    private Vector3 previousPosition; // Previous mouse position for panning
+
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+
+        // Prevent the Rigidbody from preventing the camera movement
+        if (GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (target)
+        {
+            // Orbiting
+            if (Input.GetMouseButton(1)) // Right mouse button for orbiting
+            {
+                x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+                y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+                y = ClampAngle(y, yMinLimit, yMaxLimit);
+            }
+
+            // Zooming
+            distance -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+            distance = Mathf.Clamp(distance, distanceMin, distanceMax);
+
+            // Panning
+            if (Input.GetMouseButton(0)) // Left mouse button for panning
+            {
+                Vector3 direction = previousPosition - Input.mousePosition;
+                direction = new Vector3(direction.x * panSpeed, direction.y * panSpeed, 0);
+                transform.Translate(direction);
+            }
+            previousPosition = Input.mousePosition;
+
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+            Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position + offset;
+
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+    }
+
+    // Utility function to clamp angles between a minimum and maximum value
+    private static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ Rocket Project/Assets/RocketLandingManual.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketLandingManual : MonoBehaviour
+{
+    [Header("Rocket Parameters")]
+    public float mass = 22200;
+    public float thrust = 0;
+    public float maxThrust = 1500000;
+    public float startingFuel = 200000;
+    public float thrustIncreaseRate = 1000;
+    public float fuelBurnRate = 1000;
+    public float fuel; // not to be editted in inspector
+
+    [Header("Environment Parameters")]
+    public Transform platform;
+
+    [Header("Physics Parameters")]
+    public float gravity = 9.81f;
+    public float drag = 0.1f;
+    public float angularDrag = 0.1f;
+    public float torqueAmount = 1000;
+
+    [Header("Training Parameters")]
+    public Material successMaterial;
+    public Material failMaterial;
+    public MeshRenderer platformMesh;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        fuel = startingFuel;
+        rb.mass = mass + fuel;
+        rb.drag = drag;
+        rb.angularDrag = angularDrag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeThrust();
+    }
+
+    void FixedUpdate() {
+        ApplyThrust();
+        RotationInput();
+    }
+
+    private void ApplyThrust() {
+        if (fuel > 0 && thrust > 0) {
+            Vector3 thrustVector = transform.up * thrust;
+            rb.AddForce(thrustVector);
+            fuel -= fuelBurnRate * Time.deltaTime;
+            rb.mass = mass + fuel;
+            if (fuel < 0) {
+                fuel = 0;
+            }
+        } else {
+            Debug.Log("Out of fuel");
+        }
+    }
+
+
+    private void ChangeThrust() {
+        if (Input.GetKey(KeyCode.R)) {
+            thrust += thrustIncreaseRate * Time.deltaTime;
+            if (thrust > maxThrust) {
+                thrust = maxThrust;
+            }
+            Debug.Log("Increasing Thrust: " + thrust);
+        } else if (Input.GetKey(KeyCode.F)) {
+            thrust -= thrustIncreaseRate * Time.deltaTime;
+            if (thrust < 0) {
+                thrust = 0;
+            }
+            Debug.Log("Decreasing Thrust: " + thrust);
+        }
+    }
+
+    private void RotationInput() {
+        if (Input.GetKey(KeyCode.W)) {
+            rb.AddTorque(Vector3.right * torqueAmount);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            rb.AddTorque(Vector3.left * torqueAmount);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            rb.AddTorque(Vector3.forward * torqueAmount);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            rb.AddTorque(Vector3.back * torqueAmount);
+        }
+
+        if (Input.GetKey(KeyCode.Q)) {
+            rb.AddTorque(Vector3.up * torqueAmount);
+        }
+        if (Input.GetKey(KeyCode.E)) {
+            rb.AddTorque(Vector3.down * torqueAmount);
+        }
+    }
+
+
+    // private void OnCollisionEnter(Collision other) {
+    // if (other.collider.CompareTag("goal")) {
+    //     Debug.Log("Goal");
+    //     SetReward(1f);
+    //     platformMesh.material = successMaterial;
+    //     EndEpisode();
+    // } else if (other.collider.CompareTag("wall")) {
+    //     Debug.Log("Wall");
+    //     SetReward(-1f);
+    //     platformMesh.material = failMaterial;
+    //     EndEpisode();
+    // }
+    // }
+}
