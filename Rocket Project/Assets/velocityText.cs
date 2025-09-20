a86951d87712de64c182334968ebe3300f61d534
michaeljhr <99460971+michaeljhr@users.noreply.github.com>
2025-09-20 11:59:42 -0400

    AI updates

--- Rocket Project/Assets/velocityText.cs
+++ Rocket Project/Assets/velocityText.cs
@@ -9,10 +9,14 @@ public class velocityText : MonoBehaviour
 
     public GameObject ro;
     public TMP_Text textFieldVelocity;
+    public TMP_Text textFieldAcc;
 
     public float velocity = 0f;
 
+    public float acceleration = 0f;
+
     float lastVelocity = 0f;
+    int c = 0;
 
     // Update is called once per frame
     void Update()
@@ -20,9 +24,26 @@ public class velocityText : MonoBehaviour
 
         velocity = Math.Abs(ro.GetComponent<RocketLanding>().rb.velocity.y);
         velocity = (float)Math.Round(velocity, 1);
+        if(c<50){
+            c+=1;
+        }
+        else{
+
+            acceleration = (velocity-lastVelocity)/Time.deltaTime;
+            acceleration = (float)Math.Round(acceleration, 1);
+            Debug.Log(velocity-lastVelocity);
+            lastVelocity = velocity;
+            c=0;
+        }
+
+
+
+
+
+        
 
         textFieldVelocity.text = velocity.ToString();
 
-        lastVelocity = velocity;
+
     }
 }
